b153667205a038621fe884402e8b1950137ebe92
tinhomaranholi <52422247+tinhomaranholi@users.noreply.github.com>
2020-02-19 07:42:30 -0400

    Atualização de textos

--- src/telas/home/index.js
+++ src/telas/home/index.js
@@ -7,15 +7,15 @@ import {Link} from 'react-router-dom';
 const Home = () => (
   <Column.Group centered>
     <Column size="half"  textAlign="centered" id="home">
-      <Title size={2}>Comida saudável e gostosa</Title>
-      <Title subtitle size={3}>direto na sua casa rapidamente</Title>
-      <Title subtitle size={5}>Conheça novos sabores</Title>
+      <Title size={2}>Comida Mineira, uai! </Title>
+      <Title subtitle size={3}>Direto de minas </Title>
+      <Title subtitle size={5}>Conheça O pão de queijo</Title>
       <Link to='/restaurantes'>
         <Button size="medium" color="black">
           <Icon size="small" color="white">
             <FaCrosshairs />
           </Icon>
-          <span class="has-text-white">ENCONTRAR AGORA</span>
+          <span class="has-text-white">VEM LOGO</span>
         </Button>
       </Link>
     </Column>
